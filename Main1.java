871b92bf9a64e06003648e67b4a0bccbd3c394f0
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-13 14:33:42 +0100

    change

--- Main1.java
+++ Main1.java
@@ -1,3 +1,12 @@
 public class Main1 {
     int x = 5;
+
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int maxSpeed){
+        System.out.println("Max speed is: " + maxSpeed);
+    }
 }
+
--- Second.java
+++ Second.java
@@ -2,5 +2,8 @@ public class Second {
     public static void main(String[] args) {
         Main1 myObject = new Main1();
         System.out.println(myObject.x);
+        Main1 myCar = new Main1();
+        myCar.fullThrottle();
+        myCar.speed(150);
     }
 }
