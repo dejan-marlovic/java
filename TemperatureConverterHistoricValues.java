4a0d45b67b8733d5eaf37f6e70a2d665979fa90f
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-17 11:36:24 +0100

    Exercise: "Temperaturomvandlaren"

--- TemperatureConverterHistoricValues.java
+++ TemperatureConverterHistoricValues.java
@@ -8,12 +8,25 @@ public class TemperatureConverterHistoricValues {
 
         System.out.println("Please type in Celsius temperature " +
                 "value you would like to convert to Fahrenheit:");
+        boolean addMoreValues = true;
+        while (addMoreValues) {
+            Scanner scanner = new Scanner(System.in);
+            String celsiusTemperature = scanner.nextLine();
+            double temperature = convertToFahrenheit(Double.parseDouble(celsiusTemperature));
+            temperatures.add(temperature);
+            System.out.println("Do you want to convert more values? y/n");
+            String answer = scanner.nextLine();
+            if (answer.equals("n")){
+                addMoreValues = false;
+                if(!temperatures.isEmpty()) {
+                    System.out.println(temperatures.toString());
+                }else{
+                    System.out.println("You have not added any temperature values to convert!");
 
-        Scanner scanner = new Scanner(System.in);
-        String celsiusTemperature = scanner.nextLine();
-        double temperature = convertToFahrenheit(Double.parseDouble(celsiusTemperature));
-        temperatures.add(temperature);
-        System.out.println(temperatures.toString());
+                }
+            }
+
+        }
     }
 
     static double convertToFahrenheit(double celsius) {
