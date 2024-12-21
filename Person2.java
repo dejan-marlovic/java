691b16c89a316d21a9a7a4c37db52d904e287733
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-21 11:33:16 +0100

    formatted code

--- Person2.java
+++ Person2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Person2{
+public class Person2 {
     String firstName, lastName;
     //double because can have decimal value
     double height, weight;
@@ -13,7 +13,7 @@ public class Person2{
     String streetName;
     String streetNumber;
 
-    Person2 (String firstN, String lastN, String streetN, String streetNo){
+    Person2(String firstN, String lastN, String streetN, String streetNo) {
         //String name is text
         firstName = firstN;
         lastName = lastN;
@@ -32,28 +32,32 @@ public class Person2{
 
     }
 
-    public boolean isStudent(){
+    public boolean isStudent() {
         return student;
     }
-    public String fullName(){
+
+    public String fullName() {
         return firstName + " " + lastName;
     }
-    public boolean isOfAge(int age){
+
+    public boolean isOfAge(int age) {
         return age >= 18;
     }
-    public double calculateBMI(double weight, double height ){
+
+    public double calculateBMI(double weight, double height) {
         return weight / (height * height);
 
     }
-    public String formatHeight(double height){
+
+    public String formatHeight(double height) {
         if (height < 1 && height > 0) {
-            return  (height * 100) + " cm";
-        }else if (height == 0)
+            return (height * 100) + " cm";
+        } else if (height == 0)
             return "0 cm";
-        else{
-         int meters = (int) height;
-         double fractionalPart = height - meters;
-         int centimeters  = (int) (fractionalPart * 100);
+        else {
+            int meters = (int) height;
+            double fractionalPart = height - meters;
+            int centimeters = (int) (fractionalPart * 100);
             return meters + " meter " + "and " + centimeters + " cm";
         }
     }
@@ -62,72 +66,69 @@ public class Person2{
         return streetName + " " + streetNumber;
     }
 
-    public String printFormattedInfo(){
+    public String printFormattedInfo() {
         return this.fullName()
                 + " is " + this.age + " years old "
                 + "they weigh "
-                + this.weight + " kg "+ "and are " + this.formatHeight(height) +" tall.\n"
+                + this.weight + " kg " + "and are " + this.formatHeight(height) + " tall.\n"
                 + "This means that their BMI is: "
                 + String.format("%.2f", this.calculateBMI(this.weight, this.height)) + "."
                 + " They are "
                 + (this.isStudent() ? "a student" : "not a student") + " and "
                 + (isOfAge(this.age) ? "of age. " : " not of age.")
                 + this.fullName() + " currently resides at: " + this.getStreet();
-   }
+    }
 
-   static void validateName(String name) throws Exception{
+    static void validateName(String name) throws Exception {
 
-        if(!name.matches("^[a-zA-Z]+$")) {
+        if (!name.matches("^[a-zA-Z]+$")) {
             throw new Exception("Name can not contain numbers!");
-        }
-        else if (name.length() <= 2){
+        } else if (name.length() <= 2) {
             throw new Exception("Name has to be longer than 2 characters!");
 
-        }
-        else if(name.matches("^-.*|.*-$")) {
+        } else if (name.matches("^-.*|.*-$")) {
             throw new Exception("Name can not start or end with a hyphen!");
         }
-   }
+    }
 
-   static void validateStreetName(String streetName) throws Exception{
-        if (!streetName.matches("^[a-zA-Z]+$"))
-        {
+    static void validateStreetName(String streetName) throws Exception {
+        if (!streetName.matches("^[a-zA-Z]+$")) {
             throw new Exception("Street name should only contain letters!");
         }
-   }
+    }
 
-  static void validateStreetNumber(String streetNumber) throws Exception{
+    static void validateStreetNumber(String streetNumber) throws Exception {
 
-        if (!streetNumber.matches("^[0-9]+[a-zA-Z]?$")){
+        if (!streetNumber.matches("^[0-9]+[a-zA-Z]?$")) {
             throw new Exception("Street number can only contain numbers and optionally a letter at the end!");
         }
 
-   }
-
-   static void validateSocialSecurityNumber(String socialSecurityNumber)throws Exception{
-       if (!socialSecurityNumber.matches("^[0-9]{6}-[0-9]{4}$")) {
-           throw new
-                   Exception("Social Security Number can only contain numbers and be in following format: YYMMDD-NNNN");
-       }
     }
 
-    public static void main(String[] args){
-        boolean tryAgain  = true;
+    static void validateSocialSecurityNumber(String socialSecurityNumber) throws Exception {
+        if (!socialSecurityNumber.matches("^[0-9]{6}-[0-9]{4}$")) {
+            throw new
+                    Exception("Social Security Number can only contain numbers and be in following format: YYMMDD-NNNN");
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean tryAgain = true;
         Scanner scanner = new Scanner(System.in);
         int state = 1;
         String firstName = "";
         String lastName = "";
         String streetName = "";
         String streetNumber = "";
-        do{
+        do {
 
-            try{
-                switch(state){
+            try {
+                switch (state) {
                     case 1:
                         System.out.println("Please enter first name!");
                         firstName = scanner.nextLine();
                         validateName(firstName);
-                        if (!firstName.trim().isEmpty()){
+                        if (!firstName.trim().isEmpty()) {
                             state++;
                         }
 
@@ -135,32 +136,32 @@ public class Person2{
                         System.out.println("Please enter last name!");
                         lastName = scanner.nextLine();
                         validateName(lastName);
-                        if (!lastName.trim().isEmpty()){
+                        if (!lastName.trim().isEmpty()) {
                             state++;
                         }
                     case 3:
                         System.out.println("Please enter street name!");
                         streetName = scanner.nextLine();
                         validateStreetName(streetName);
-                        if (!streetName.trim().isEmpty()){
+                        if (!streetName.trim().isEmpty()) {
                             state++;
                         }
                     case 4:
                         System.out.println("Please enter street number!");
                         streetNumber = scanner.nextLine();
                         validateStreetNumber(streetNumber);
-                        if (!streetNumber.trim().isEmpty()){
+                        if (!streetNumber.trim().isEmpty()) {
                             tryAgain = false;
                         }
                 }
 
-            }catch(Exception e){
-                System.out.println(e.toString());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
                 System.out.println("Do you want to try again? y/n");
                 String answer = scanner.nextLine();
                 tryAgain = answer.equals("y");
             }
-        }while(tryAgain);
+        } while (tryAgain);
 
         Person2 person = new Person2(firstName, lastName, streetName, streetNumber);
         System.out.println("I have created person object ");
