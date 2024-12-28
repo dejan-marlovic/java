b6389cf84de1e2cb4a10d13cf676c536b0f4edb5
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-28 16:44:56 +0100

    formatted code

--- mypack/MyPackageClass.java
+++ mypack/MyPackageClass.java
@@ -14,7 +14,7 @@
  * javac -d . MyPackageClass.java
  * <p>
  * This forces the compiler to create the "mypack" package.
- *
+ * <p>
  * The -d keyword specifies the destination for where to save the class file. You can use any
  * directory name, like c:/user (windows), or, if you want to keep the package within the same
  * directory, you can use the dot sign ".", like in the example above.
